ef63de0abb6473a8f96589bb69a0c619acbd2b9b
Matthew Shaver <matt@autonomiq.io>
2018-08-19 23:58:48 -0700

    Plugin can login to autonomiq service

--- src/main/java/io/jenkins/plugins/autonomiq/AutonomiqBuilder.java
+++ src/main/java/io/jenkins/plugins/autonomiq/AutonomiqBuilder.java
@@ -2,14 +2,12 @@ package io.jenkins.plugins.autonomiq;
 
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.google.gson.Gson;
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.FilePath;
-import hudson.model.Item;
+import hudson.model.*;
 import hudson.util.FormValidation;
-import hudson.model.AbstractProject;
-import hudson.model.Run;
-import hudson.model.TaskListener;
 import hudson.tasks.Builder;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.util.ListBoxModel;
@@ -19,25 +17,32 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
 import javax.servlet.ServletException;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundSetter;
 
 public class AutonomiqBuilder extends Builder implements SimpleBuildStep {
 
+    private String aiqUrl;
     private String login;
     private String password;
     private String project;
 
+    private final String authenticatePath = ":8005/authenticate/basic";
 
     @DataBoundConstructor
-    public AutonomiqBuilder(String login, String password, String project) {
+    public AutonomiqBuilder(String aiqUrl, String login, String password, String project) {
+        this.aiqUrl = aiqUrl;
         this.login = login;
         this.password = password;
         this.project = project;
     }
 
+    public String getAiqUrl() { return aiqUrl; }
     public String getLogin() {
         return login;
     }
@@ -53,6 +58,10 @@ public class AutonomiqBuilder extends Builder implements SimpleBuildStep {
 //    }
 
     @DataBoundSetter
+    public void setAiqUrl(String aiqUrl) {
+        this.aiqUrl = aiqUrl;
+    }
+    @DataBoundSetter
     public void setLogin(String login) {
         this.login = login;
     }
@@ -67,17 +76,91 @@ public class AutonomiqBuilder extends Builder implements SimpleBuildStep {
 
     @Override
     public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
-//        if (useFrench) {
-//            listener.getLogger().println("Bonjour, " + name + "!");
-//        } else {
-//            listener.getLogger().println("Hello, " + name + "!");
-//        }
+
+        boolean ok = true;
+        PrintStream log = listener.getLogger();
+
+        log.println("Logging in to Autonomiq service: " + aiqUrl);
+        log.println("as user: " + login);
+
+        AuthenticateUserBody authBody = new AuthenticateUserBody(login, password);
+
+        Gson gson = new Gson();
+        String authJson = gson.toJson(authBody);
+
+        // log.println("auth json " + authJson);
+
+        HttpURLConnection con = null;
+
+        try {
+
+            URL myurl = new URL(aiqUrl + authenticatePath);
+            con = (HttpURLConnection) myurl.openConnection();
+
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", "Java client");
+            con.setRequestProperty("Content-Type", "application/json");
+
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(authJson.getBytes());
+            }
+
+            StringBuilder content;
+
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()))) {
+
+                String line;
+                content = new StringBuilder();
+
+                while ((line = in.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+
+            log.println("Login Response:");
+            log.println(content.toString());
+
+        } catch (Exception e) {
+            ok = false;
+            log.println(getExceptionTrace(e));
+        } finally {
+
+            if (con != null) con.disconnect();
+        }
+
+
+        if (ok) {
+            run.setResult(Result.SUCCESS);
+        } else {
+            run.setResult(Result.FAILURE);
+        }
+
+    }
+
+    private String getExceptionTrace(Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String sStackTrace = sw.toString();
+        return sStackTrace;
     }
 
     @Symbol("greet")
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
+        public FormValidation doCheckAiqUrl(@QueryParameter String value, @QueryParameter String aiqUrl)
+                throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error(Messages.AutonomiqBuilder_DescriptorImpl_errors_missingAiqUrl());
+            if (! (value.startsWith("http://") || value.startsWith("https://")))
+                return FormValidation.warning(Messages.AutonomiqBuilder_DescriptorImpl_errors_notUrl());
+
+            return FormValidation.ok();
+        }
         public FormValidation doCheckLogin(@QueryParameter String value, @QueryParameter String login)
                 throws IOException, ServletException {
             if (value.length() == 0)
@@ -180,4 +263,21 @@ public class AutonomiqBuilder extends Builder implements SimpleBuildStep {
 //        return FormValidation.ok();
 //    }
 
+    class AuthenticateUserBody {
+        private String username;
+        private String password;
+
+        public AuthenticateUserBody(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
 }
